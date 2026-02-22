445266d6a2c2386bdeb028d808ea6c97a19bce03
Ranjita <138755332+ranjita07@users.noreply.github.com>
2026-02-22 19:43:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
 
+const registerForm = document.querySelector('#register-form');
+const phoneInput = document.querySelector('input[name="business_phone_number"]');
+const otpPhoneInput = document.querySelector('.form-box.otp input[name="business_phone_number"]');
+
+const submitBtn = registerForm.querySelector('button');
+
+submitBtn.disabled = true;
+submitBtn.innerText = "Processing...";
 
 registerBtn.addEventListener('click', () => {
     container.classList.add('active');
@@ -10,3 +18,29 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 });
+
+registerForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(registerForm);
+
+    try{
+
+        const response = await fetch(registerForm.action, {
+        method: 'POST',
+        body: formData
+     });
+
+    if (response.ok) {
+        otpPhoneInput.value = phoneInput.value;
+        container.classList.add('otp-active');
+    } else {
+        alert("Registration Failed");
+    }
+
+   } catch (error) {
+        alert("Network Error");
+    }
+});
+
+document.querySelector('.form-box.otp').classList.add('otp-error');
